03cd74634d1f6dc1c9bab655f28f5f0de5401d24
Aman Shukla <amanks7400@gmail.com>
2022-07-27 19:06:03 +0530

    Add Discover Section

---
+++ src/constants/data.js
@@ -0,0 +1,14 @@
+export const discover = [
+    {
+        img: require('../../assets/images/discover/bg1.jpeg')
+    },
+    {
+        img: require('../../assets/images/discover/bg2.jpeg')
+    },
+
+    {
+        img: require('../../assets/images/discover/bg3.jpeg')
+    },
+
+
+]
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity, ScrollView, ImageBackground } from 'react-native'
 import React from 'react'
+import greetings from '../../src/utils/greetings'
+import Roboto from "../../src/constants/fonts"
+import Icon from 'react-native-vector-icons/Ionicons'
+import { discover } from '../constants/data'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{
+      flex: 1,
+      backgroundColor: "white"
+    }}>
+
+      <ScrollView alwaysBounceVertical={false} vertical={true} showsVerticalScrollIndicator={false}>
+
+        {/* Header */}
+        <HomeHeader />
+
+        {/* Discover */}
+        <Discover />
+
+        {/* Recently Played */}
+        {/* Popular */}
+
+      </ScrollView>
+
+    </SafeAreaView>
   )
 }
 
 export default Home
+
+
+function HomeHeader() {
+
+  const greeting = greetings()
+
+  return (
+    <View style={{
+      paddingHorizontal: 16,
+      paddingVertical: 16,
+      flexDirection: "row",
+      justifyContent: "space-between",
+    }}>
+      {/* Greeting */}
+
+      <View>
+        <Text style={{
+          fontFamily: Roboto.bold,
+          fontSize: 24
+        }}>
+          Hey Aman
+        </Text>
+        <Text style={{
+          fontFamily: Roboto.regular,
+        }}>
+          {greeting}
+        </Text>
+      </View>
+      {/* Search Button */}
+      <TouchableOpacity>
+        <Icon name="search" size={24} />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+function Discover() {
+  return (
+    <>
+      <Text style={{ fontSize: 16, marginBottom: 4, marginLeft: 16, fontFamily: Roboto.medium }}>
+        Discover this week's latest songs
+      </Text>
+
+
+      <ScrollView bounces={false} horizontal={true} style={{
+        marginLeft: 16
+      }} showsHorizontalScrollIndicator={false}>
+        {
+          discover.map((item, i) =>
+
+            <ImageBackground
+              key={i}
+              imageStyle={{
+                borderRadius: 16
+
+              }}
+              source={item.img}
+              style={{
+                width: 360,
+                marginRight: 32,
+                height: 240,
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: 16
+
+              }}>
+              <Text style={{ fontSize: 32, color: "white", fontFamily: Roboto.bold }}>
+                Discover Card {i}
+              </Text>
+            </ImageBackground>
+          )
+        }
+      </ScrollView>
+
+    </>
+
+  )
+}
