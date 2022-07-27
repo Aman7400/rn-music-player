bcb2dad4f266e2bb6a5e65904ab8d9f06cf553fa
Aman Shukla <amanks7400@gmail.com>
2022-07-27 18:05:12 +0530

    Add Roboto Font

--- App.js
+++ App.js
@@ -1,14 +1,35 @@
 
 import * as React from 'react';
 import MainNavigation from './src/navigation/MainNavigation';
-
+import {
+  useFonts,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto';
+import { ActivityIndicator, View } from 'react-native';
 
 
 
 function App() {
+
+  let [fontsLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <View style={{flex:1}}>
+      <ActivityIndicator style={{flex: 1}} />
+    </View>;
+  }
+
   return (
     <>
-     <MainNavigation />
+      <MainNavigation />
     </>
   );
 }
---
+++ src/constants/fonts.js
@@ -0,0 +1,6 @@
+export default {
+    light : 'Roboto_300Light',
+    regular : 'Roboto_400Regular',
+    medium : 'Roboto_500Medium',
+    bold : 'Roboto_700Bold'
+}
