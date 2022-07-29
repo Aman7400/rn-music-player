35d144978451f81d8f4204164de3ec26d1ec6a03
Aman Shukla <amanks7400@gmail.com>
2022-07-29 18:06:26 +0530

    Update Discover.js

--- src/components/home/Discover.js
+++ src/components/home/Discover.js
@@ -1,14 +1,15 @@
-import { ImageBackground, ScrollView, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { ImageBackground, ScrollView, Text,TouchableOpacity } from "react-native";
 import { discover } from "../../constants/data";
 import Roboto from "../../constants/fonts";
 
 export default function Discover() {
+  const navigation = useNavigation()
   return (
     <>
       <Text style={{ fontSize: 16, marginBottom: 4, marginLeft: 16, fontFamily: Roboto.medium }}>
         Discover this week's latest songs
       </Text>
-
 
       <ScrollView bounces={false} horizontal={true} style={{
         marginLeft: 16
@@ -16,23 +17,27 @@ export default function Discover() {
         {
           discover.map((item, i) =>
 
-            <ImageBackground
-              key={i}
-              imageStyle={{
-                borderRadius: 16
-              }}
-              source={item.img}
-              style={{
-                width: 360,
-                marginRight: i < discover.length - 1 ? 32 : 16,
-                height: 240,
-                justifyContent: "center",
-                alignItems: "center",
-              }}>
-              <Text style={{ fontSize: 32, color: "white", fontFamily: Roboto.bold }}>
-                Discover Card {i}
-              </Text>
-            </ImageBackground>
+            <TouchableOpacity key={i} onPress={() => navigation.navigate("SongList", { list: "Discover" })} >
+              <ImageBackground
+
+                imageStyle={{
+                  borderRadius: 16
+                }}
+                source={item.img}
+                style={{
+                  width: 360,
+                  marginRight: i < discover.length - 1 ? 32 : 16,
+                  height: 240,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>
+                <Text style={{ fontSize: 32, color: "white", fontFamily: Roboto.bold }}>
+                  Discover Card {i}
+                </Text>
+              </ImageBackground>
+            </TouchableOpacity>
+
+
           )
         }
       </ScrollView>
